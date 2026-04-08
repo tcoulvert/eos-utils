64cfe68de95f4aee663b46b8338f5fa8164c21f9
Thomas Sievert <tcsievert@gmail.com>
2026-04-07 23:58:44 -0500

    fixing bugs

--- src/eos_utils/__init__.py
+++ src/eos_utils/__init__.py
@@ -1,4 +1,9 @@
 from eos_utils.get_voms import get_voms
+from eos_utils.get_redirector import get_redirector
+
 from eos_utils.copy_eos import copy_eos 
+
 from eos_utils.load_file_eos import load_file_eos
 from eos_utils.save_file_eos import save_file_eos
+
+from eos_utils.glob_eos import glob_eos
--- src/eos_utils/glob_eos.py
+++ src/eos_utils/glob_eos.py
@@ -1,21 +1,32 @@
 import glob
-import json
-import subprocess
-
-import pandas as pd
+import os
 
 from eos_utils.get_redirector import get_redirector
+from eos_utils.get_voms import get_voms
 
 def glob_eos(filepath: str, recursive: bool=False, include_hidden: bool=False):
     if filepath.startswith('root://'):
         redirector, filepath = get_redirector(filepath)
+        proxy = get_voms()
 
-        globsplit_filepath = filepath.split('**')
+        if '*' in filepath:
+            base_filepath, glob_paths = filepath.split('*')[0], filepath.split('*')[1:]
+        else:
+            base_filepath, glob_paths = filepath, ['']
 
-        list_cmd = 'ls' + (' -R' if recursive else '') + (' -a' if include_hidden else '')
-        out = subprocess.run(['xrdfs', redirector, 'ls -R' if recursive else 'ls', filepath], capture_output=True, text=True)
-        copy_eos(filepath, tmp_file)
-        object_ = load_file(return_type, tmp_file); 
+        list_cmnd = 'ls' + (' -R' if recursive else '') + (' -a' if include_hidden else '')
+        os.system(f"xrdfs {redirector} {list_cmnd} {base_filepath} > temp.txt")
+
+        globs = []
+        with open('temp.txt', 'r') as f:
+            for line in f:
+                stdline = line.strip()
+                match_line = stdline[stdline.find(base_filepath)+len(base_filepath):]; save_line = True
+                for glob_path in glob_paths:
+                    if glob_path in match_line: 
+                        match_line = match_line[match_line.find(glob_path)+len(glob_path):]
+                    else: save_line = False; break
+                if save_line: globs.append(redirector+stdline)
     else:
-        object_ =  glob.glob(filepath, recursive=recursive, include_hidden=include_hidden)
-    return object_
+        globs =  glob.glob(filepath, recursive=recursive, include_hidden=include_hidden)
+    return globs
