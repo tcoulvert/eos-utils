0196ab97a36e41de6e516717e30272b458c5cf39
Thomas Sievert <tcsievert@gmail.com>
2026-06-09 14:24:18 -0500

    moved sleep back to end to avoid missing blocking

--- src/eos_utils/watch_tmp.py
+++ src/eos_utils/watch_tmp.py
@@ -7,7 +7,6 @@ def watch_tmp(tmp_filepath: str, n: int=1, sleep_time: float=10, timeout: float=
     start_watch_time = time.perf_counter()
     time_elapsed = lambda : time.perf_counter() - start_watch_time
     while True:
-        time.sleep(sleep_time)
         
         if times_blocked == n and not blocked: return True
         if time_elapsed > timeout: return False
@@ -16,4 +15,5 @@ def watch_tmp(tmp_filepath: str, n: int=1, sleep_time: float=10, timeout: float=
             os.rename(tmp_filepath, tmp_filepath); blocked = False
         except:
             times_blocked += 1; blocked = True
+        time.sleep(sleep_time)
     
