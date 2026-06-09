2e17088d10f320892527bf4288f73a0197c98aef
Thomas Sievert <tcsievert@gmail.com>
2026-06-09 14:00:24 -0500

    added sleep first in watch

--- src/eos_utils/watch_tmp.py
+++ src/eos_utils/watch_tmp.py
@@ -1,4 +1,4 @@
-import fcntl
+import os
 import time
 
 def watch_tmp(tmp_filepath: str, n: int=1, sleep_time: float=10, timeout: float=600):
@@ -7,14 +7,13 @@ def watch_tmp(tmp_filepath: str, n: int=1, sleep_time: float=10, timeout: float=
     start_watch_time = time.perf_counter()
     time_elapsed = lambda : time.perf_counter() - start_watch_time
     while True:
+        time.sleep(sleep_time)
         
         if times_blocked == n and not blocked: return True
         if time_elapsed > timeout: return False
 
         try: 
-            with open(tmp_filepath, 'r') as f: fcntl.flock(fn.fileno(), fcntl.LOCK_NB)
-            blocked = False
+            os.rename(tmp_filepath, tmp_filepath); blocked = False
         except:
             times_blocked += 1; blocked = True
-        time.sleep(sleep_time)
     
